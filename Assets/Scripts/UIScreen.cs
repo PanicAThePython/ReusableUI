4d2d011c459958065221b5e4f820f23cf89e4a76
PanicAThePython <nataliaweise@gmail.com>
2020-06-16 16:13:30 -0300

    Enviando projeto

---
+++ Assets/Scripts/UIScreen.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(CanvasGroup))]
+public class UIScreen : MonoBehaviour
+{
+    [Header("Main Properties")]
+    public Selectable startSelectable;
+
+    
+
+    [Header("Screen Events")]
+    public UnityEvent onStartScreen = new UnityEvent();
+    public UnityEvent onCloseScreen = new UnityEvent();
+
+
+    public Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        if (startSelectable)
+        {
+            EventSystem.current.SetSelectedGameObject(startSelectable.gameObject);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public virtual void StartScreen()
+    {
+        if(onStartScreen != null)
+        {
+            onStartScreen.Invoke();
+        }
+
+        HandleAnimator("show");
+    }
+    public virtual void CloseScreen()
+    {
+        if (onCloseScreen != null)
+        {
+            onCloseScreen.Invoke();
+        }
+        HandleAnimator("hide");
+    }
+
+    void HandleAnimator(string aTrigger)
+    {
+        if (animator)
+        {
+            animator.SetTrigger(aTrigger);
+        }
+    }
+}
---
+++ Assets/Scripts/UISystem.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class UISystem : MonoBehaviour
+{
+    [Header("Main Properties")]
+    public UIScreen startScreen;
+
+    [Header("System Events")]
+    public UnityEvent onSwitchedScreen = new UnityEvent();
+
+    [Header("Fader Properties")]
+    public Image fader;
+    public float fadeInDuration = 1f, fadeOutDuration = 1f;
+
+    private Component[] screens = new Component[0];
+
+    private UIScreen previousScreen;
+    public UIScreen PreviousScreen { get { return previousScreen; } }
+
+
+    private UIScreen currentScreen;
+    public UIScreen CurrentScreen { get { return currentScreen; } }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screens = GetComponentsInChildren<UIScreen>(true);
+        InitializeScreens();
+
+        if (startScreen)
+        {
+            SwitchScreen(startScreen);
+        }
+
+        if (fader)
+        {
+            fader.gameObject.SetActive(true);
+        }
+        FadeIn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FadeIn() 
+    {
+        if (fader)
+        {
+            fader.CrossFadeAlpha(0f, fadeInDuration, false);
+        }
+    }
+    public void FadeOut() 
+    {
+        if (fader)
+        {
+            fader.CrossFadeAlpha(1f, fadeOutDuration, false);
+        }
+    }
+
+    public void SwitchScreen(UIScreen aScreen)
+    {
+        if (aScreen)
+        {
+            if (currentScreen)
+            {
+                currentScreen.CloseScreen();
+                previousScreen = currentScreen;
+            }
+            currentScreen = aScreen;
+            currentScreen.gameObject.SetActive(true);
+            currentScreen.StartScreen();
+
+            if (onSwitchedScreen != null)
+            {
+                onSwitchedScreen.Invoke();
+            }
+        }
+    }
+
+    public void GoToPreviousScreen()
+    {
+        if (previousScreen)
+        {
+            SwitchScreen(previousScreen);
+        }
+    }
+
+    public void LoadScene(int index)
+    {
+        StartCoroutine(WaitToLoadScene(index));
+    }
+
+    IEnumerator WaitToLoadScene(int index)
+    {
+        yield return null;
+    }
+
+    void InitializeScreens()
+    {
+        foreach(var screen in screens)
+        {
+            screen.gameObject.SetActive(true);
+        }
+
+    }
+}
